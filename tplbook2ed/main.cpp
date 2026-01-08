bace3afe928240ad73d2c25b9c5d61c3c1dfc517
刘会平 <will_369@outlook.com>
2026-01-08 16:17:46 +0800

    [feat] Chapter4 Variadic Templates

--- tplbook2ed/main.cpp
+++ tplbook2ed/main.cpp
@@ -3,6 +3,7 @@
 #include "../PartI-TheBasics/Chapter1-FunctionTemplates.h"
 #include "../PartI-TheBasics/Chapter2-ClassTemplates.h"
 #include "../PartI-TheBasics/Chapter3-NontypeTemplateParameters.h"
+#include "../PartI-TheBasics/Chapter4-VariadicTemplates.h"
 
 //#include <cstdio>
 
@@ -12,7 +13,8 @@ int main()
 
 	//func_template_example();
 	//class_template_example();
-	nontype_template_param_example();
+	//nontype_template_param_example();
+	variadic_template_example();
 
 	return 0;
 }
