5b09ca2aea777ca6c495cd27edcda715c4cbbf67
刘会平 <will_369@outlook.com>
2026-06-18 14:55:00 +0800

    [feat] add some code snapet about concepts and generic programmiing

--- PartIII-TemplatesAndDesign/Chpater19-ImplementingTraits.cpp
+++ PartIII-TemplatesAndDesign/Chpater19-ImplementingTraits.cpp
@@ -1,6 +1,7 @@
 #include "Chpater19-ImplementingTraits.h"
 
-#include <iostream>
+#include <complex>
+
 
 namespace implementing_traits {
 
@@ -109,5 +110,95 @@ void implementing_traits_example() {
 				<< mem_tpl_vs_tpl_tpl_param::accum(num, num + 5) 
 				<< "\n";
 		}
+
+		// 19.2.4 Accumulation with General Iterators
+		{
+			using namespace accum_with_general_iterators;
+
+			int num[] = { 1,2,3,4,5,6,7 };
+			std::cout << "the summation of the integer values is "
+				<< accum(num, num + 5)
+				<< "\n";
+		}
+	}
+	// 19.3 Type Functions
+	{
+		using namespace type_function;
+
+		std::cout << "TypeSize<int>::value = " << TypeSize<int>::value << "\n";
+		// 19.3.1 Element Types
+		{
+			using namespace elem_types;
+			std::vector<bool> s;
+			printElementType(s);
+			int arr[2];
+			printElementType(arr);
+
+			std::vector<int> vi = { 1,23,3,4 };
+			std::cout << "SumOfElments(vi) = " << sumOfElements(vi) << "\n";
+		}
+		// 19.3.2 Transformation Traits
+		{
+			using namespace transformation_traits;
+
+		}
+	}
+
+	// Concepts and Generic Programming
+	{
+		using namespace concept_generic_programming;
+
+		std::list<int> l = { 1,2,2,3,4,5,6,76,7,7,8,8,8 };
+		auto curl = l.begin();
+		my_advance(curl, 4);
+
+		std::cout << *curl << "\n";
+
+		std::vector<int> v = { 1,2,2,3,4,5,6,76,7,7,8,8,8 };
+		auto curv = v.begin();
+		my_advance(curv, 4);
+		std::cout << *curv << "\n";
+
+#if 1
+		static_assert(Equality_comparable<int>,"int is not comparable");
+		static_assert(Equality_comparable<int, double>);
+		static_assert(std::equality_comparable<int>);
+		static_assert(std::equality_comparable_with<int, double>, "int is not comparable double");
+#else
+		//static_assert(Equality_comparable<S>, "S is not comparable");// fails
+		//static_assert(Equality_comparable<int, std::string>, "int is not comparable to string");// fails
+#endif
+
+#if 1
+		auto b1 = cmp(2, 3);// ok
+		auto b3 = equal(2 + 3i, 3 + 4i); // ok
+#else
+		auto b0 = cmp(std::cout, std::cin);//error,ostream doesn't support ==
+		auto b2 = cmp(2 + 3i, 3 + 4i);//error,complex<double> doesn't support<
+#endif
+
+		auto x1 = twice(5);
+		std::string s= "Hello";
+		//auto x2 = twice(s);// error
+		auto x3 = thrice(s);//ok
+
+		std::cout << x1 << " " << x3 << "\n";
+	
+		auto ch1 = open_channel("foo");// works with whatever open_channel() returns
+		//Arithmetic auto ch2 = open_channel("foo");// error: a channel is not arithmetic
+		Channel auto ch3 = open_channel("foo");// Ok, assuming Channel is an appropriate concept
+											// and open_channel() returns one
+
+		std::cout << ch1 << "  " << ch3 << "\n";
+
+
+		print("hello", 3, 4);
+		print_f("hello",',', "world",',', 2017);
+
+		auto total = sum_f(2, 3, 4, 4, 5, 65.6);
+		std::cout << total << "\n";
+
+		auto xxx = foo(3);
+
 	}
 }	
--- PartIII-TemplatesAndDesign/Chpater19-ImplementingTraits.h
+++ PartIII-TemplatesAndDesign/Chpater19-ImplementingTraits.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <list>
+#include <typeinfo>
+#include <type_traits>
+#include <ranges>
 
 namespace implementing_traits {
 
@@ -251,6 +258,312 @@ namespace implementing_traits {
 				return total;
 			}
 		}
+		// 19.2.3 Combing Multiple Policies and/or Traits
+		// 19.2.4 Accumulation with General Iterators
+		namespace accum_with_general_iterators {
+			template<typename Iter>
+			auto accum(Iter start, Iter end) {
+				using VT = typename std::iterator_traits<Iter>::value_type;
+				VT total{};//assume this actually create a zero value
+				while (start != end) {
+					total += *start;
+					++start;
+				}
+				return total;
+			}
+		}
+	}
+	// 19.3 Type Functions
+	namespace type_function {
+
+		template<typename T>
+		struct TypeSize {
+			static std::size_t const value = sizeof(T);
+		};
+
+		// 19.3.1 Element Types
+		namespace elem_types {
+			template<typename T>
+			struct ElementT;// primary template
+
+
+			template<typename T>
+			struct ElementT<std::vector<T>> {// partial specialization for std::vector
+				using Type = T;
+			};
+
+			template<typename T>
+			struct ElementT<std::list<T>> {// partial specialization for std::list
+				using Type = T;
+			};
+
+			template<typename T, std::size_t N>
+			struct ElementT<T[N]> {// partial specialization for array of known bounds
+				using Type = T;
+			};
+			template<typename T>
+			struct ElementT<T[]> {// partial specialization for array of known bounds
+				using Type = T;
+			};
+
+			template<typename T>
+			void printElementType(T const& c) {
+				std::cout << "Container of " << typeid(typename ElementT<T>::Type).name()
+					<< " element\n";
+			}
+
+			template<typename T>
+			using ElementType = typename ElementT<T>::Type;
+
+			template<typename C>
+			ElementType<C> sumOfElements(C const& c) {
+				ElementType<C> total{};
+				for (auto const& i : c) {
+					total += i;
+				}
+				return total;
+			}
+		}
+		// 19.3.2 Transformation Traits
+		namespace transformation_traits {
+
+			// Removing References
+			template<typename T>
+			struct RemoveReferenceT {
+				using Type = T;
+			};
+			template<typename T>
+			struct RemoveReferenceT<T&> {
+				using Type = T;
+			};
+			template<typename T>
+			struct RemoveReferenceT<T&&> {
+				using Type = T;
+			};
+			template<typename T>
+			using RemoveReference = typename RemoveReferenceT<T>::Type;
+
+			// Adding References
+#if 0
+			template<typename T>
+			struct AddLValueReferenceT {
+				using Type = T&;
+			};
+
+			template<typename T>
+			struct AddRValueReferenceT {
+				using Type = T&&;
+			};
+
+			template<typename T>
+			using AddLValueReference = typename AddLValueReferenceT<T>::Type;
+
+
+			template<typename T>
+			using AddRValueReference = typename AddRValueReferenceT<T>::Type;
+#else
+			// apply reference collapsing rule(either if lvalue result it lvalue, both rvalue result is rvalue)
+			template<typename T>
+			using AddLValueReference = T&;
+			template<typename T>
+			using AddRvalueReference = T&&;
+#endif
+
+
+			inline void might_throw(bool b)noexcept
+			{
+				if (b) {
+					throw 42;
+				}
+			}
+		}
+	}
+
+
+	// Concepts and Generic Programming
+	namespace concept_generic_programming {
+
+		template<std::random_access_iterator iter>
+		void my_advance(iter& p, int n) {
+			p += n;
+			std::cout << __FUNCTION__<<"(random_access_iterator)" << "\n";
+		}
+		
+#if 1
+		template<std::forward_iterator iter>
+		void my_advance(iter& p, int n) {
+			while (n--) {
+				++p;
+			}
+			std::cout << __FUNCTION__ << "(forward_iterator)" << "\n";
+		}
+#else
+
+		template<std::forward_iterator iter>
+			requires requires(iter p, int i) { p[i], p + i; }
+		void my_advance(iter& p, int n) {
+			p += n;
+			std::cout << __FUNCTION__ << "(forward_iterator requires())" << "\n";
+		}
+#endif
+
+		// defintion of concepts
+
+		template<typename B>
+		concept Boolean =
+			requires(B x, B y) {
+				{ x == true };
+				{ y = false };
+				{ x = (x == y) };
+				{ x = (x != y) };
+				{ x = !x };
+				{ x = (x = y) };
+		};
+
+		struct S {};
+#if 0
+		template<typename T>
+		concept Equality_comparable =
+			requires(T a, T b) {
+				{ a == b } -> Boolean;
+				{ a != b } -> Boolean;
+		};
+#else
+
+		// handle nonhomogeneous comparision
+		template<typename T,typename T2 = T>
+		concept Equality_comparable =
+			requires(T a, T2 b) {
+				{ a == b }->Boolean;
+				{ a != b }->Boolean;
+				{ b == a }->Boolean;
+				{ b != a }->Boolean;
+		};
+#endif
+
+		template<typename T, typename U=T>
+		concept Number =
+			requires(T x, U y) {
+			x + y;x - y;x* y;x / y;
+			x += y; x -= y;x *= y;x /= y;
+			x = x;
+			x = 0;
+		};
+
+		template<typename T,typename U=T>
+		concept Arithmetic = Number<T, U>&& Number<U, T>;
+
+#if 0
+		template<typename S>
+		concept Sequence = requires(S a) {
+			typename range_value_t<S>;
+			typename iterator_t<S>;
+
+			{ a.begin() }->std::same_as<iterator_t<S>>;
+			{ a.end() }->std::same_as<iterator_t<S>>;
+
+				requires std::input_iterator<iterator_t<S>>;
+				requires std::same_as<range_value_t<S>, iter_value_t<S>>;
+		};
+
+		template<typename S>
+		concept Sequence = std::input_range<S>;
+#endif
+
+	template<std::equality_comparable T>
+	bool cmp(T a, T b) {
+		return a < b;
+	}
+	template<std::equality_comparable T>
+	bool equal(T a, T b) {
+		return a == b;
+	}
+
+	auto twice(Arithmetic auto x) {// just for Arithmetic Number
+		return x * x;
+	}
+	auto thrice(auto x) {// for anything with a+
+		return x + x + x;
+	}
+	template<typename T>
+	concept Channel =
+		requires(T a){
+		std::same_as<T, std::string>;
+	};
+
+	inline Channel auto open_channel(std::string const& name) {
+		return std::string{ name + "channel" };
+	}
+
+	// Variadic template
+	template<typename T>
+	concept Printable = requires(T t) { std::cout << t; };// just one operation
+
+	template<Printable T, Printable...Tail>
+	void print(T head, Tail...tail) {
+		std::cout << head << " ";
+		if constexpr (sizeof...(tail) > 0) {
+			print(tail...);
+		}
+		else
+		{
+			std::cout << "\n";
+		}
+	}
+
+	// Fold Expression
+
+	template<Printable...T>
+	void print_f(T&&...args) {
+		(std::cout << ...<< args) << "\n";
+	}
+
+	template<Number...T>
+	int sum_f(T...v) {// right fold
+		
+		return (v + ... + 0);// add all elements of v starting with 0
+		// the first element to be added is the "rightmost"
+		// (v[0] + (v[1]+(v[2]+(v[3] +0)))
+	}
+	template<Number...T>
+	int sum2(T...v) {// left fold
+		return (0 + ... + v);//add all elements of v to 0
+		// (((((0+v[0])+v[1])+v[2])+v[3])
+	}
+
+	// Forwarding argument
+
+	// combine fold expression and forwarding
+	//Scenario A: Logging/Validating each argument before forwarding
+	template<typename... Args>
+	void log_and_forward(Args&&... args) {
+		// Fold expression with comma operator:
+		// It calls log() for each argument IN ORDER (C++17 guarantees left-to-right),
+		// while perfectly preserving rvalue/lvalue status via std::forward.
+		(log(std::forward<Args>(args)), ...);
+
+		// Then forward the whole pack to another function.
+		target_function(std::forward<Args>(args)...);
+	}
+	//Scenario B: Pushing multiple perfectly-forwarded elements into a container
+	template<typename T, typename... Args>
+	void multi_push(std::vector<T>& vec, Args&&... args) {
+		// Fold expression over the comma operator.
+		// Calls vec.push_back(arg1), vec.push_back(arg2) ...
+		// std::forward ensures if arg1 is a temporary, it gets moved into the vector.
+		(vec.push_back(std::forward<Args>(args)), ...);
+	}
+	//Scenario C: Passing forwarded args to different functions
+	template<typename... Args>
+	void process_all(Args&&... args) {
+		// Calls handler1 with arg1, handler2 with arg2, etc.
+		(handler(std::forward<Args>(args)), ...);
+	}
+
+	constexpr auto foo(int v) {
+		return v * v;
+	}
+
 	}
 }
 
